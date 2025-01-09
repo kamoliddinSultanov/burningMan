d79f395140436c88cd6a2755b9bcacb3e81c7f8e
Kamoliddin Sultanov <sultanovk97@gmail.com>
2025-01-10 03:37:04 +0500

    aaa

--- log/admin_functions.php
+++ log/admin_functions.php
@@ -5,23 +5,22 @@ class AdminFunctions
 {
     private $dbConnection;
 
-    // Constructor to initialize the database connection
     public function __construct()
     {
         $this->dbConnection = (new DB())->getDbConnection();
     }
 
-    // Verify that the session is for admin users
     public function verifyAdminSession()
     {
-        session_start();
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
         if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'admin') {
             header("location: index.php");
             exit();
         }
     }
 
-    // Function to create an event
     public function createEvent($title, $description, $date, $created_by)
     {
         $stmt = $this->dbConnection->prepare("INSERT INTO events (title, description, date, created_by) VALUES (?, ?, ?, ?)");
@@ -29,21 +28,17 @@ class AdminFunctions
         return $stmt->execute();
     }
 
-    // Function to delete an event
     public function deleteEvent($event_id)
     {
-        // Delete connected records from user_events
         $stmt = $this->dbConnection->prepare("DELETE FROM user_events WHERE event_id = ?");
         $stmt->bind_param('i', $event_id);
         $stmt->execute();
 
-        // Delete the event itself
         $stmt = $this->dbConnection->prepare("DELETE FROM events WHERE id = ?");
         $stmt->bind_param('i', $event_id);
         return $stmt->execute();
     }
 
-    // Function to edit an event
     public function editEvent($event_id, $title, $description, $date)
     {
         $stmt = $this->dbConnection->prepare("UPDATE events SET title = ?, description = ?, date = ? WHERE id = ?");
@@ -51,14 +46,12 @@ class AdminFunctions
         return $stmt->execute();
     }
 
-    // Function to fetch all events
     public function fetchEvents()
     {
         $result = $this->dbConnection->query("SELECT * FROM events");
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    // Function to fetch user-event data
     public function fetchUserEventData()
     {
         $query = "
@@ -79,12 +72,12 @@ class AdminFunctions
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 }
-?>
 
 
-<!--
-    Author: Kamoliddin Sultanov
-    File purpose: operations that can perform only admin
 
--->
+
+// Author: Kamoliddin Sultanov
+// File purpose: operations that can perform only admin
+
+
 
